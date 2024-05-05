e6b60afef15aafcd70f1a1543b7b3fd551cdaeec
apmfree78 <amit@profitswami.com>
2024-05-05 15:09:28 -0400

    refactor: refactor user data update after user action detected from event logs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bigdecimal::BigDecimal;
-use eth_liquadation::exchanges::aave_v3::data::{
-    AaveUserData, Generate, HealthFactor, PricingSource,
+use eth_liquadation::{
+    events::aave_events::scan_and_update_aave_events,
+    exchanges::aave_v3::data::{AaveUserData, Generate, HealthFactor, PricingSource},
 };
 use ethers::providers::{Provider, Ws};
 use std::sync::Arc;
@@ -15,26 +16,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut aave_user_list = AaveUserData::get_users(&client).await?;
     // println!(" user list {:#?} ", aave_user_list);
 
-    for user in &aave_user_list {
-        let calculated_health_factor = user
-            .get_health_factor_from_(PricingSource::UniswapV3, &client)
-            .await?;
+    scan_and_update_aave_events(&mut aave_user_list, &client).await?;
+    // for user in &aave_user_list {
+    //     let calculated_health_factor = user
+    //         .get_health_factor_from_(PricingSource::UniswapV3, &client)
+    //         .await?;
 
-        // TODO - 10% of graphl data is corrupted and unrealiable for user token amount ( and maybe
-        // the tokens themselves) please clean out this corrupted data
-        println!(" user data {:#?}", user);
-        println!("----------------------------------------");
-        println!(" user heath factor {}", user.health_factor);
-        println!(
-            " user heath factor oracle {}",
-            user.get_health_factor_from_(PricingSource::AaveOracle, &client)
-                .await?
-        );
-        println!(" user heath factor ft {}", calculated_health_factor);
-        if calculated_health_factor > BigDecimal::from(10) * &user.health_factor {
-            println!(" user data {:#?}", user);
-        }
-    }
+    //     // TODO - 10% of graphl data is corrupted and unrealiable for user token amount ( and maybe
+    //     // the tokens themselves) please clean out this corrupted data
+    //     println!(" user data {:#?}", user);
+    //     println!("----------------------------------------");
+    //     println!(" user heath factor {}", user.health_factor);
+    //     println!(
+    //         " user heath factor oracle {}",
+    //         user.get_health_factor_from_(PricingSource::AaveOracle, &client)
+    //             .await?
+    //     );
+    //     println!(" user heath factor ft {}", calculated_health_factor);
+    //     if calculated_health_factor > BigDecimal::from(10) * &user.health_factor {
+    //         println!(" user data {:#?}", user);
+    //     }
+    // }
     Ok(())
 
     // scan published blocks for aave user events where account data is updated
